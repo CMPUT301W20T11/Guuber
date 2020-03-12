2052e337fa3c3f4186509d184349c52466ffb7ef
DoppleGengar <33990134+DoppleGengar@users.noreply.github.com>
2020-03-12 16:21:46 -0600

    edit test

--- Guuber/app/src/test/java/com/example/guuber/GuuDbTests.java
+++ Guuber/app/src/test/java/com/example/guuber/GuuDbTests.java
@@ -2,12 +2,16 @@ package com.example.guuber;
 
 //import static org.junit.Assert.*;
 import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class GuuDbTests
-{
+public class GuuDbTests {
+    private GuuDb db = new GuuDb();
+
 
     @Test
-    public void TestUserSetup()
-    {
-        GuuDb db = new db;
+    public void TestUserSetup() {
+        db.setUpUser("123@123.ca","Luke","Atme","LukeAtme","780 780 7807","rider");
+        db.findUser("123@123.ca");
     }
+
+}
