ac1caea3d1ff4df9648e0d723d3960f448881000
DoppleGengar <33990134+DoppleGengar@users.noreply.github.com>
2020-03-12 23:30:29 -0600

    Finish adding the java docs to the vehicle class

--- Guuber/app/src/main/java/com/example/guuber/Vehicle.java
+++ Guuber/app/src/main/java/com/example/guuber/Vehicle.java
@@ -22,32 +22,60 @@ public class Vehicle {
 
 	/**
 	 * Get the person the car is registered to
-	 * @return - Driver
+	 * @return - register driver
 	 */
 	public String getReg() { return reg; }
 
+	/**
+	 * set the person the car is registered to
+	 * @param reg - set who is register to the vehicle
+	 */
 	public void setReg(String reg) { this.reg = reg; }
 
+	/**
+	 * Get the make of the vehicle
+	 * @return - make
+	 */
 	public String getMake() {
 		return make;
 	}
 
+	/**
+	 * set the make of the vehicle
+	 * @param make - the make of the vehicle
+	 */
 	public void setMake(String make) {
 		this.make = make;
 	}
 
+	/**
+	 * Get the model of the vehicle
+	 * @return - model
+	 */
 	public String getModel() {
 		return model;
 	}
 
+	/**
+	 * set the model of the vehicle
+	 * @param model - the model of the vehicle
+	 */
 	public void setModel(String model) {
 		this.model = model;
 	}
 
+	/**
+	 * Get the color of the vehicle
+	 * @return - color
+	 */
 	public String getColor() {
 		return color;
 	}
 
+	/**
+	 * set the color of the vehicle
+	 * @param color - the color of the vehicle
+	 */
 	public void setColor(String color) {
 		this.color = color;
 	}
