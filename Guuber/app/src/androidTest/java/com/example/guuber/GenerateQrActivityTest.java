b0b560df1b5c454e4e0ccd7076671e3ea3312074
Leah Copeland <59579748+leah-is-offline@users.noreply.github.com>
2020-03-12 23:48:23 -0700

    scanQRActivity and QrActivity robotium testing

---
+++ Guuber/app/src/androidTest/java/com/example/guuber/GenerateQrActivityTest.java
@@ -0,0 +1,50 @@
+package com.example.guuber;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class GenerateQrActivityTest {
+
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<QrActivity> rule =
+            new ActivityTestRule<>(QrActivity.class,true,true);
+
+    /**
+     * * runs before all tests and creates solo instance.
+     * @throws Exception
+     **/
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+        solo.waitForActivity(QrActivity.class, 1000);
+    }
+
+    /**
+     * check that generate QR
+     * stays in Activity
+     */
+    @Test
+    public void onGenerateButtonClick(){
+        solo.waitForActivity(QrActivity.class, 1000);
+        solo.clickOnText("Generate");
+        solo.assertCurrentActivity("Activity should stay in QrActivity", QrActivity.class);
+    }
+
+    /**
+     * closes the activity after each test
+     * @throws Exception
+     * */
+    @After
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
+}
