f562534722f24e6b13968ac1e623f6be5b78d183
mdziubin <mdziubin@ualberta.ca>
2020-03-18 16:47:03 -0600

    Added QR scanning proof of concept

--- Guuber/app/src/main/java/com/example/guuber/scanQrActivity.java
+++ Guuber/app/src/main/java/com/example/guuber/scanQrActivity.java
@@ -1,46 +1,64 @@
 package com.example.guuber;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Bitmap;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.journeyapps.barcodescanner.BarcodeEncoder;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 /**
  * This class contains the activity the driver
  * will be directed to upon selecting "scan QR"
  * in their menu
  * it enables their ability to scan a QR
+ * Source: https://www.youtube.com/watch?v=Fe7F4Jx7rwo
  */
 public class scanQrActivity extends AppCompatActivity {
-    private ImageView scanQrImage;
-    private Button scanQrButton;
 
 
-    @Override
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qr);
+		Button scan_button = findViewById(R.id.scan_button);
+        final Activity activity = this;
 
-        scanQrImage = findViewById(R.id.scan_qr_image);
-        scanQrButton = findViewById(R.id.scan_qr_button);
-
-        /**display the back button**/
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        scan_button.setOnClickListener(v -> {
+			IntentIntegrator integrator = new IntentIntegrator(activity);
+			integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+			integrator.setPrompt("Scan");
+			integrator.setCameraId(0);
+			integrator.setBeepEnabled(false);
+			integrator.setBarcodeImageEnabled(false);
+			integrator.initiateScan();
+		});
 
     }
 
-    /**implement logic here for what you want to
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+		IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+		if(result != null){
+			if(result.getContents() == null){
+				Toast.makeText(this, "You cancelled the scanning", Toast.LENGTH_LONG).show();
+			}else{
+				Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+			}
+		}
+		else{
+			super.onActivityResult(requestCode, resultCode, data);
+		}
+	}
+
+	/**implement logic here for what you want to
      * happen upon back button press**/
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
