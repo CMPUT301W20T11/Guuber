a0d9c41f40643576c506d8c36da0e0591c481ef9
mdziubin <mdziubin@ualberta.ca>
2020-04-03 11:23:03 -0600

    Cleaned LoginActivty code

--- Guuber/app/src/main/java/com/example/guuber/LoginActivity.java
+++ Guuber/app/src/main/java/com/example/guuber/LoginActivity.java
@@ -1,23 +1,17 @@
 package com.example.guuber;
 
-import androidx.annotation.NonNull;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
-import android.Manifest;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
 
 import com.example.guuber.model.User;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -28,23 +22,19 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 /**
- * class to implement the Login Activity
+ * Class to implement the Login Activity
  * includes google sign in and ability
  * to log in as rider or driver
+ * Source: https://github.com/firebase/quickstart-android
  */
 public class LoginActivity extends AppCompatActivity implements RegisterFragment.OnFragmentInteractionListener {
 	private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 10;
@@ -56,14 +46,13 @@ public class LoginActivity extends AppCompatActivity implements RegisterFragment
 	private FirebaseAuth mAuth;
 	private static final String TAG = "LoginActivity";
 	private GoogleSignInClient mGoogleSignInClient;
-	//private RadioGroup radioGroup;
 
 	private FirebaseFirestore db = FirebaseFirestore.getInstance();
 	private DocumentReference uRef;
 
 	private static final int RC_SIGN_OUT = 1000;
 
-	//location client
+	// Location client
 	private FusedLocationProviderClient fusedLocationClient;
 	Location currLocation;
 
@@ -73,7 +62,6 @@ public class LoginActivity extends AppCompatActivity implements RegisterFragment
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		SignInButton signInButton = findViewById(R.id.sign_in_button);
-		//radioGroup = findViewById(R.id.radio_group);
 
 		// Configure sign-in to request the user's ID, email address, and basic
 		// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -87,38 +75,29 @@ public class LoginActivity extends AppCompatActivity implements RegisterFragment
 
 		mAuth = FirebaseAuth.getInstance();
 
-		signInButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (v.getId() == R.id.sign_in_button) {
-					signIn();
-				}
+		signInButton.setOnClickListener(v -> {
+			if (v.getId() == R.id.sign_in_button) {
+				signIn();
 			}
 		});
 
 
-		/******INITIALIZING LOCATION CLIENT*********/
+		// INITIALIZING LOCATION CLIENT
 		fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 		fusedLocationClient.getLastLocation()
-				.addOnSuccessListener(this, new OnSuccessListener<Location>() {
-					@Override
-					public void onSuccess(Location location) {
-						android.util.Log.i("Holy shit", "location is null");
-						if (location != null) {
-							currLocation = location;
-						}
+				.addOnSuccessListener(this, location -> {
+					Log.i("Bad", "location is null");
+					if (location != null) {
+						currLocation = location;
 					}
 				});
-		/**********************************************/
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
 		FirebaseUser currentUser = mAuth.getCurrentUser();
-		currentUser = null; // TODO delete when login activity is perfectly working
 		updateUI(currentUser);
-
 	}
 
 
@@ -164,40 +143,34 @@ public class LoginActivity extends AppCompatActivity implements RegisterFragment
 
 		AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
 		mAuth.signInWithCredential(credential)
-				.addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-					@Override
-					public void onComplete(@NonNull Task<AuthResult> task) {
-						if (task.isSuccessful()) {
-							// Google sign in success
-							Log.d(TAG, "signInWithCredential:success");
-							final FirebaseUser user = mAuth.getCurrentUser();
+				.addOnCompleteListener(this, task -> {
+					if (task.isSuccessful()) {
+						// Google sign in success
+						Log.d(TAG, "signInWithCredential:success");
+						final FirebaseUser user = mAuth.getCurrentUser();
 
-							// Check if user has registered
-							uRef = db.collection("Users").document(user.getEmail());
-							uRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-								@Override
-								public void onSuccess(DocumentSnapshot documentSnapshot) {
-									if(!documentSnapshot.exists()){
-										Log.d(TAG, "User not found in DB: " + documentSnapshot.getData());
-										RegisterFragment registerFragment = RegisterFragment.newInstance(user.getUid(), user.getEmail());
-										registerFragment.show(getSupportFragmentManager(), "Register");
-									}else{
-										Log.d(TAG, "User info pulled from DB " + documentSnapshot.getData());
-										updateUI(user);
-									}
-								}
-							});
-						} else {
-							// If sign in fails, display a message to the user.
-							Context context = getApplicationContext();
-							CharSequence text = "Google sign in failed";
-							int duration = Toast.LENGTH_SHORT;
+						// Check if user has registered
+						uRef = db.collection("Users").document(user.getEmail());
+						uRef.get().addOnSuccessListener(documentSnapshot -> {
+							if(!documentSnapshot.exists()){
+								Log.d(TAG, "User not found in DB: " + documentSnapshot.getData());
+								RegisterFragment registerFragment = RegisterFragment.newInstance(user.getUid(), user.getEmail());
+								registerFragment.show(getSupportFragmentManager(), "Register");
+							}else{
+								Log.d(TAG, "User info pulled from DB " + documentSnapshot.getData());
+								updateUI(user);
+							}
+						});
+					} else {
+						// If sign in fails, display a message to the user.
+						Context context = getApplicationContext();
+						CharSequence text = "Google sign in failed";
+						int duration = Toast.LENGTH_SHORT;
 
-							Toast toast = Toast.makeText(context, text, duration);
-							toast.show();
+						Toast toast = Toast.makeText(context, text, duration);
+						toast.show();
 
-							updateUI(null);
-						}
+						updateUI(null);
 					}
 				});
 	}
@@ -216,64 +189,36 @@ public class LoginActivity extends AppCompatActivity implements RegisterFragment
 
 		// Google sign out
 		mGoogleSignInClient.signOut().addOnCompleteListener(this,
-				new OnCompleteListener<Void>() {
-					@Override
-					public void onComplete(@NonNull Task<Void> task) {
-						updateUI(null);
-					}
-				});
-	}
-
-	private void revokeAccess() {
-		// Firebase sign out
-		mAuth.signOut();
-
-		// Google revoke access
-		mGoogleSignInClient.revokeAccess().addOnCompleteListener(this,
-				new OnCompleteListener<Void>() {
-					@Override
-					public void onComplete(@NonNull Task<Void> task) {
-						updateUI(null);
-					}
-				});
+				task -> updateUI(null));
 	}
 
 	private void updateUI(FirebaseUser user) {
-		// TODO check what type of user is logged in and display its appropriate homepage
-
-		//int radioButtonID = radioGroup.getCheckedRadioButtonId();
-		//View radioButton = radioGroup.findViewById(radioButtonID);
-		//int signInType = 0;
 		User loggedUser = null;
 		Context context = LoginActivity.this;
 		if (user != null) {
 			// Populate the singleton
 			uRef = db.collection("Users").document(user.getEmail());
-			uRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-				@Override
-				public void onSuccess(DocumentSnapshot documentSnapshot) {
-					User loggedUser = documentSnapshot.toObject(User.class);
-					((UserData) (getApplicationContext())).setUser(loggedUser);
-					Intent homeScreen;
-					int signInType = 0;
-					if (loggedUser.getRider() == 1) {
-						signInType = 1;
-					}
-
-					// Go to home screen depending on user type
-					if (signInType == 1) {
-						//if user is a Rider
-						homeScreen = new Intent(context, MapsRiderActivity.class);
-					} else {
-						//else user is a driver
-						homeScreen = new Intent(context, MapsDriverActivity.class);
-					}
-					startActivityForResult(homeScreen, RC_SIGN_OUT);
-					//startActivity(homeScreen);
+			uRef.get().addOnSuccessListener(documentSnapshot -> {
+				User loggedUser1 = documentSnapshot.toObject(User.class);
+				((UserData) (getApplicationContext())).setUser(loggedUser1);
+				Intent homeScreen;
+				int signInType = 0;
+				if (loggedUser1.getRider() == 1) {
+					signInType = 1;
 				}
+
+				// Go to home screen depending on user type
+				if (signInType == 1) {
+					//if user is a Rider
+					homeScreen = new Intent(context, MapsRiderActivity.class);
+				} else {
+					//else user is a driver
+					homeScreen = new Intent(context, MapsDriverActivity.class);
+				}
+				startActivityForResult(homeScreen, RC_SIGN_OUT);
 			});
 		} else {
-			/**if the user has never registered before we have to get permissions**/
+			// If the user has never registered before we have to get permissions
 			checkUserPermissions();
 			}
 
@@ -333,12 +278,9 @@ public class LoginActivity extends AppCompatActivity implements RegisterFragment
 				"exploit information that makes you vulnerable. If you have chosen to not be asked again, " +
                 "please visit your app setting and grant us your location permissions")
 				.setCancelable(false)
-                .setPositiveButton("Got It!", new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						checkUserPermissions();
-						dialog.dismiss();
-					}
+                .setPositiveButton("Got It!", (dialog, which) -> {
+					checkUserPermissions();
+					dialog.dismiss();
 				});
 	final AlertDialog alert = builder.create();
         alert.show();
